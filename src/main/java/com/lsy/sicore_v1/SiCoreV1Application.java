9f824196b3249ebfe2a2750b943f640fdea2044a
이상엽 <infightersy@naver.com>
2025-12-24 09:03:32 +0900

    first commit

---
+++ src/main/java/com/lsy/sicore_v1/SiCoreV1Application.java
@@ -0,0 +1,13 @@
+package com.lsy.sicore_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiCoreV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiCoreV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsy/sicore_v1/SiCoreV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.sicore_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiCoreV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
